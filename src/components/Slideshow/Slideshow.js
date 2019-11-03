9ac572c93e8c3f361be97810aa51611a6478d950
Christopher Sannar <chris.sannar.dev@gmail.com>
2019-11-04 01:57:31 +0700

    Sooo tired
    Just got a kick and added more fancy stuff. Need to clean it up and actually test what I added

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -3,9 +3,6 @@ import './Slideshow.css';
 
 export default class Slideshow extends React.Component {
 
-  // pin object: {note: "Funny memes", image: { original ... }, id: "351140102197375629"}
-  // original: {url: "https://i.pinimg.com/originals/2b/2d/2e/2b2d2e2b0295d529c58f31527f9a6dcd.jpg", width: 1052, height: 1300}
-
   constructor(props) {
     super(props);
     this.state = {
@@ -13,11 +10,36 @@ export default class Slideshow extends React.Component {
       pins: this.props.pins,
     }
 
-    this.centerPin = React.createRef();
+    this.Center = React.createRef();
+    this.Prev = React.createRef();
+    this.Next = React.createRef();
+    this.Exit = React.createRef();
     
     this.close = this.close.bind(this);
     this.handleKey = this.handleKey.bind(this);
+    this.handleMouse = this.handleMouse.bind(this);
     window.addEventListener(`keydown`, this.handleKey);
+    window.addEventListener(`mousemove`, this.handleMouse);
+
+  }
+
+  // Just a nice blend in when the app opens up
+  componentDidMount() {
+    this.Prev.current.style.opacity = 1;
+    this.Next.current.style.opacity = 1;
+    this.Exit.current.style.opacity = 1;
+
+    setTimeout(() => {
+      this.Prev.current.style.opacity = 0;
+      this.Next.current.style.opacity = 0;
+      this.Exit.current.style.opacity = 0;
+    }, 3000);
+  }
+
+  // Clears the event listeners so they don't carry out
+  componentWillUnmount() {
+    window.removeEventListener(`mousemove`, this.handleMouse);
+    window.removeEventListener(`keydown`, this.handleKey);
   }
 
   // Closes the overlay
@@ -27,6 +49,7 @@ export default class Slideshow extends React.Component {
 
   // Moves the index of the current pin
   changePin(indexChange) {
+    this.props.addMorePins(this.state.index);
     if (indexChange + this.state.index < 0) {
       this.setState({ index: this.state.pins.length - 1 });
     } else {
@@ -35,7 +58,7 @@ export default class Slideshow extends React.Component {
   }
   
   handleKey(event) {
-    console.log(this.centerPin.current);
+    // console.log(this.centerPin.current);
     switch(event.keyCode) {
       case 37:  // Left
         event.preventDefault();
@@ -62,19 +85,61 @@ export default class Slideshow extends React.Component {
     }
   }
 
+  // A nice bouse effect that makes the buttons fade when they aren't needed
+  handleMouse(event) {
+
+    // Prev
+    let left = this.Prev.current.getBoundingClientRect().right + 50;
+    if (event.x < left) {
+      this.Prev.current.style.opacity = 1;
+    } else {
+      this.Prev.current.style.opacity = 0;
+    }
+    
+    // Next
+    let right = this.Next.current.getBoundingClientRect().left - 50;
+    if (event.x > right) {
+      this.Next.current.style.opacity = 1;
+    } else {
+      this.Next.current.style.opacity = 0;
+    }
+
+    // Exit
+    let etop = this.Exit.current.getBoundingClientRect().bottom + 50;
+    // let eleft = this.Exit.current.getBoundingClientRect().left - 50;
+    if (event.x > right && event.y < etop) {
+      this.Exit.current.style.opacity = 1;
+    } else {
+      this.Exit.current.style.opacity = 0;
+    }
+  }
+
   render () {
     return (
       <div className="overlay">
-        <span className="exit" onClick={this.close}>X</span>
+        <span 
+          className="exit" 
+          onClick={ this.close }
+          ref={ this.Exit }>X</span>
         <div className="slideshow">
-          <span className="prev" onClick={ () => this.changePin(-1) }>PREV</span>
+          <span 
+            className="prev" 
+            onClick={ () => this.changePin(-1) }
+            ref={ this.Prev }>
+              PREV
+          </span>
           <div 
             className="scrollable" 
             onClick={ () => this.changePin(1) } 
-            ref={ this.centerPin }>
+            ref={ this.Center }>
             <img className="center-pin" src={ this.state.pins[this.state.index].image.original.url } alt="Current Pin"/>
           </div>
-          <span className="next" onClick={ () => this.changePin(1) }>NEXT</span>
+          <span 
+            className="next" 
+            onClick={ () => this.changePin(1) }
+            ref={ this.Next }>
+              NEXT
+          </span>
         </div>
       </div>
     )
